bddbab2beb16a243cdc52f2cf0912a0e81a3295e
maxaponpatapon@gmail.com <maxaponpatapon@gmail.com>
2024-07-06 21:10:19 +0300

    correction on homework

--- src/components/app/component.jsx
+++ src/components/app/component.jsx
@@ -1,52 +1,35 @@
 import { restaurants} from "../../../materials/mock";
 import { Restaurant } from "../restaurant/component";
 import { Layout } from "../layout/component";
-import React from  "react"
+import { useState } from "react";
 import "/src/css/Tabs.css"
 
 export const App = () => {
-    const [currentId, setId] = React.useState(restaurants[0].id);
+    const [currRestaurantIndx, setCurrRestaurantIndx] = useState(0);
     function changeActiveTab(event)
     {
-        setId(event.target.value);
+        setCurrRestaurantIndx(event.target.value);
     }
-
-    React.useEffect(() => {
-        Array.from(document.getElementsByClassName("tabBtn")).forEach((element) => {
-            if (element.value != currentId)
-            {
-                element.className = element.className.replace(" active", "")
-            }
-            else
-            {
-                element.className += " active"
-                console.log(element.value);
-            }
-        });
-
-        Array.from(document.getElementsByClassName("tabContent")).forEach((element) => {
-            if (element.id != currentId)
-            {
-                element.style.display = "none";
-            }
-            else
-            {
-                element.style.display = "block"
-            }
-        })
-    })
 
     return (
         <Layout>
             <div className="tab">
                 {restaurants.map((restaurant) => (
-                   <button className="tabBtn" onClick={changeActiveTab} value={restaurant.id} disabled={!Boolean(restaurant.menu?.length)}>{restaurant.name}</button>
+                    <button className={restaurants.indexOf(restaurant) == currRestaurantIndx ? "tabBtn active" : "tabBtn"} 
+                            onClick={changeActiveTab} 
+                            value={restaurants.indexOf(restaurant)} 
+                            disabled={!Boolean(restaurant.menu?.length)}>
+                        {restaurant.name}
+                    </button>
                 ))}
             </div>
             <div>
-                {restaurants.map((restaurant) => (
-                    <Restaurant id={restaurant.id} name={restaurant.name} menu={restaurant.menu} reviews={restaurant.reviews}/>
-                ))}   
+                {
+                    <Restaurant id={restaurants[currRestaurantIndx].id} 
+                                name={restaurants[currRestaurantIndx].name} 
+                                menu={restaurants[currRestaurantIndx].menu} 
+                                reviews={restaurants[currRestaurantIndx].reviews}/>
+                }
             </div>
         </Layout>
     )
