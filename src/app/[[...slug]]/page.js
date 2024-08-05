d4df3303cc6b07f86f87ab65bb365da5a7b90d18
maxaponpatapon@gmail.com <maxaponpatapon@gmail.com>
2024-08-05 22:39:21 +0300

    DZ10

---
+++ src/app/[[...slug]]/page.js
@@ -0,0 +1,6 @@
+import App from "./app";
+
+export default function Page()
+{//Остановился на 34 минуте
+    return <App/>
+}
--- src/main.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { App } from "../src/components/app/component";
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
